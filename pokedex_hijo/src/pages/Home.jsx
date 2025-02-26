dd9cbfe0af6274a2763cc4d0b514076a279bfa70
Katherine Delgado <ksdelgado31@gmail.com>
2025-02-25 21:30:28 -0600

    Mapping data

--- pokedex_hijo/src/pages/Home.jsx
+++ pokedex_hijo/src/pages/Home.jsx
@@ -1,18 +1,30 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 import {usePokemonStore} from '../store/PokemonStore';
 import {useQuery} from '@tanstack/react-query';
+import {ClimbingBoxLoader} from "react-spinners"
 
 export function Home() {
   const {fetchPokemons, pokemons} = usePokemonStore();
   const {isLoading, isError, data, error} = useQuery({queryKey: ['mostrarpokemons'], queryFn: fetchPokemons});
 
   if (isLoading) {
-    return <span>Loading...</span>
+    return <ClimbingBoxLoader color="#89ce6b" size={30} speedMultiplier={2} />
+  }
+
+  if (isError) {
+    return <span>{error.message}</span>
   }
 
   return (
-    <div>
-      Home
-    </div>
+    <Container>
+      {
+        data.results.map((item,i) => (
+          <span key={i}>{item.name}</span>
+        ))
+      }
+      <ClimbingBoxLoader color="#89ce6b" size={30} speedMultiplier={2} />
+    </Container>
   )
 }
+
+const Container = styled.div``;
--- pokedex_hijo/src/store/PokemonStore.jsx
+++ pokedex_hijo/src/store/PokemonStore.jsx
@@ -3,12 +3,13 @@ import {create} from 'zustand'
 export const usePokemonStore = create((set, get) => ({
   pokemons: [],
   fetchPokemons: async () => {
-    const endpoint = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+    const endpoint = 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0'
     const response = await fetch(endpoint);
 
     const data = await response.json();
     set({
           pokemons: data,
         });
+    return data;
   },
 }))
